4edb6654f79ace8c8e418b862e33555c0518ba4e
shivaneej <shivaneej02@gmail.com>
2022-09-10 01:57:24 -0400

    Basic GET and POST using Firebase

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+import collections
 import json
+from turtle import pos
 from flask import Flask, request
+import firebase_admin
+from firebase_admin import credentials, db
+
+FIREBASE_URL = 'https://mas-programming-9f4f3-default-rtdb.firebaseio.com/'
+PATH_TO_SERVICE_ACC_KEY = 'mas-programming-9f4f3-firebase-adminsdk-ir27a-0a40c33de8.json'
 
 app = Flask(__name__)
 
+cred = credentials.Certificate(PATH_TO_SERVICE_ACC_KEY)
+firebase_admin.initialize_app(cred, {'databaseURL' : FIREBASE_URL})
+ref = db.reference('/posts')
+
 @app.route('/posts', methods=['GET'])
 def query_posts():
-    posts = [1, 2, 3]
+    posts = ref.order_by_child('timestamp').get()
+    posts = collections.OrderedDict(reversed(list(posts.items())))    
     return posts
 
 @app.route('/posts', methods=['POST'])
 def create_post():
-    post_info = json.loads(request.data)
+    request_data = json.loads(request.data)
+    post_info = {
+        'name': request_data['name'],
+        'description': request_data['description'],
+        'price': request_data['price'],
+        'category': request_data['category'],
+        'image_url': request_data['image_url'],
+        'mobile': request_data['mobile'],
+        'timestamp': {'.sv': 'timestamp'}
+    }
+    ref.push().set(post_info)
     return post_info
 
 
